b37df2316f067cfabc64c740ce130b940e73493f
Nick <mojiotok167@gmail.com>
2024-04-09 14:50:39 +0300

    update homework for 4 lesson

--- S4_HW/main.go
+++ S4_HW/main.go
@@ -1,30 +1,215 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
+	"strconv"
 )
 
+type User struct {
+	Id      int     `json:"id"`
+	Name    string  `json:"name"`
+	Age     int     `json:"age"`
+	Friends []*User `json:"friends"`
+}
+
+type Network struct {
+	network map[int]*User
+}
+
 func main() {
+
+	n := Network{make(map[int]*User)}
+
+	nick := User{
+		Id:      1,
+		Name:    "Nick",
+		Age:     27,
+		Friends: nil,
+	}
+
+	vasya := User{
+		Id:      2,
+		Name:    "Vasya",
+		Age:     37,
+		Friends: nil,
+	}
+
+	n.network[nick.Id] = &nick
+
+	n.network[vasya.Id] = &vasya
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	})
 
-	r.Mount("/books", BookRoutes())
+	r.Post("/create/", n.Create)
+	r.Get("/user/", n.GetAll)
+	r.Post("/make_friends/{source}-{target}", n.MakeFriends)
+	r.Delete("/delete/{id}", n.Delete)
+	r.Get("/friends/{id}", n.GetFriends)
+	r.Put("/update_age/{id}-{age}", n.UpdateAge)
 
 	http.ListenAndServe("localhost:8080", r)
 }
 
-func BookRoutes() chi.Router {
-	r := chi.NewRouter()
-	bookHandler := BookHandler{}
-	r.Get("/", bookHandler.ListBooks)
-	r.Post("/", bookHandler.CreateBook)
-	r.Get("/{id}", bookHandler.GetBooks)
-	r.Put("/{id}", bookHandler.UpdateBook)
-	r.Delete("/{id}", bookHandler.DeleteBook)
-	return r
+func (n *Network) Create(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer r.Body.Close()
+	n.network[user.Id] = &user
+	jsonUser, err := json.Marshal(user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonUser)
+}
+
+func (n *Network) GetAll(w http.ResponseWriter, r *http.Request) {
+	result := make([]User, 0, len(n.network))
+	for _, user := range n.network {
+		result = append(result, *user)
+	}
+	resultJson, err := json.Marshal(result)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(resultJson)
+}
+
+func (n *Network) MakeFriends(w http.ResponseWriter, r *http.Request) {
+	sourceIdStr := chi.URLParam(r, "source")
+	sourceId, err := strconv.Atoi(sourceIdStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	targetIdStr := chi.URLParam(r, "target")
+	targetId, err := strconv.Atoi(targetIdStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if _, ok := n.network[sourceId]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	if _, ok := n.network[targetId]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	sourceName := n.network[sourceId].Name
+	targetName := n.network[targetId].Name
+
+	n.UpdateFriends(n.network[sourceId], n.network[targetId])
+
+	msg := fmt.Sprintf("%v и %v теперь друзья", targetName, sourceName)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(msg))
+}
+
+func (n *Network) Delete(w http.ResponseWriter, r *http.Request) {
+	idStr := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if _, ok := n.network[id]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	deletedName := n.network[id].Name
+	msg := fmt.Sprintf("%v удалён!", deletedName)
+
+	delete(n.network, id)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(msg))
+
+}
+
+func (n *Network) GetFriends(w http.ResponseWriter, r *http.Request) {
+	idStr := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if _, ok := n.network[id]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	resultFriends := make([]*User, 0, len(n.network))
+
+	resultFriends = append(resultFriends, n.network[id].Friends...)
+
+	resultJson, err := json.Marshal(resultFriends)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(resultJson)
+
+}
+
+func (n *Network) UpdateAge(w http.ResponseWriter, r *http.Request) {
+	idStr := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	ageStr := chi.URLParam(r, "age")
+	age, err := strconv.Atoi(ageStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if _, ok := n.network[id]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	n.network[id].Age = age
+	msg := fmt.Sprintf("Возраст пользователя %v успешно обновлен!", n.network[id].Name)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(msg))
+}
+
+func (n *Network) UpdateFriends(user1, user2 *User) {
+	n.network[user1.Id].Friends = append(n.network[user1.Id].Friends, n.network[user2.Id])
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Integration_and_Parallelism_In_Go
 
 go 1.22
+
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
